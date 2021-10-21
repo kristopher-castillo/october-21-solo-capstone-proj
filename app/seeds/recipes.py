9b4be34e57faeedf33d44da8b8f973702f59d6be
Kristopher C. Castillo <kccastillo2@gmail.com>
2021-10-21 12:12:13 -0400

    Debugged second recipe seeder

--- app/seeds/recipes.py
+++ app/seeds/recipes.py
@@ -7,7 +7,7 @@ def seed_recipes():
     yield_amount=4,
     completion_time=35,
     user_id=1
-    ),
+    )
   demo_recipe_2 = Recipe(
     title="Soy-Butter Basted Scallops With Wilted Greens and Sesame",
     description="This simple dish was inspired by a recipe for steamed scallop and butter rice found in “Donabe: Classic and Modern Japanese Clay Pot Cooking” (Ten Speed, 2015) by Naoko Takei Moore and Kyle Connaughton. Here, sweet sea scallops are seared in a hot pan and basted with melted butter and soy sauce to finish cooking. Tender greens are sautéed in garlic oil, then the scallops are placed on top and everything is drizzled with the remaining soy-butter and a bit of sesame oil. Finish the dish with a good squeeze of lime, thinly sliced scallions and a smattering of sesame seeds. It’s wonderful served over steamed white rice, so be sure to get that on the stove before you begin cooking the scallops, as the rest of the meal comes together in no time at all.",
