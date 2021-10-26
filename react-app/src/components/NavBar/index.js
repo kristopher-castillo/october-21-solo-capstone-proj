a610175556a9c3e71d568488393cce8fb59f6bb6
Kristopher C. Castillo <kccastillo2@gmail.com>
2021-10-26 13:07:04 -0400

    added sessionlinks and padding

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,46 +1,62 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 
 import "./NavBar.css"
 
 const NavBar = () => {
+  const sessionUser = useSelector((state) => state.session.user);
+
+  let sessionLinks;
+
+  if (sessionUser) {
+    sessionLinks = (
+      <div className="user-container">
+        <LogoutButton />
+      </div>
+      
+    )
+  } else {
+    sessionLinks = (
+      <div className="user-container">
+        <NavLink to='/login' exact={true} activeClassName='active'>
+          Login
+        </NavLink>
+
+        <NavLink to='/sign-up' exact={true} activeClassName='active'>
+          Sign Up
+        </NavLink>
+      </div>
+    )
+  }
+
   return (
-    <nav>
+    <nav className="nav-bar">
       <div className="nav-logo-container">
         <NavLink to='/' exact={true} activeClassName='logo-link'>
           {/* <button className="nav-logo-button"></button> */}
           <p className="nav-logo-text">|Cooking</p>
         </NavLink>
       </div>
-      <li>
-        <NavLink to='/login' exact={true} activeClassName='active'>
-          Login
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to='/sign-up' exact={true} activeClassName='active'>
-          Sign Up
-        </NavLink>
-      </li>
-      <li>
+      <div>
         <NavLink to='/users' exact={true} activeClassName='active'>
           Users
         </NavLink>
-      </li>
-      <li>
+      </div>
+      <div>
         <NavLink to='/recipes/1' exact={true} activeClassName='active'>
           Recipe
         </NavLink>
-      </li>
-      <li>
+      </div>
+      <div>
         <NavLink to='/recipes/new' exact={true} activeClassName='active'>
           New Recipe
         </NavLink>
-      </li>
-      <li>
-        <LogoutButton />
-      </li>
+      </div>
+      <div>
+        {sessionLinks}
+      </div>
     </nav>
   );
 }
