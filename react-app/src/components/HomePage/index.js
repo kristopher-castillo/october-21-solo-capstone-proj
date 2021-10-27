4be56a4dc3388c66d669657b1c05399a4566d41a
Kristopher C. Castillo <kccastillo2@gmail.com>
2021-10-27 01:52:11 -0400

    Added random recommended recipes to home page

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,12 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory, useParams, Link } from "react-router-dom";
+import { getRecipes } from "../../store/recipe";
+import { getImages } from "../../store/image";
 import Footer from "../Footer";
 
 import './HomePage.css'
 
 const HomePage = () => {
+  const recipes = useSelector((state) => state.recipes?.recipes?.all_recipes);
+  const images = useSelector((state) => state.images?.images?.all_images);
+  const dispatch = useDispatch();
 
+  useEffect(() => dispatch(getRecipes()), [dispatch]);
+  useEffect(() => dispatch(getImages()), [dispatch]);
+
+  let recommendedRecipes;
+  if (recipes) {
+    recommendedRecipes = recipes.sort(() => 0.5 - Math.random()).slice(0, 3)
+  }
 
   return (
     <>
@@ -31,6 +43,25 @@ const HomePage = () => {
         <div className="home-body-container">
           <h1 className="home-what-to-cook">What To Cook This Week</h1>
           <hr className="what-to-cook-break"></hr>
+          <div className="recommended-recipes-container">
+            {recommendedRecipes?.map((recipe) => (
+              <Link to={`/recipes/${recipe?.id}`}>
+                <div className="recommended-recipe-card" key={recipe?.id}>
+                  <img
+                    className="recommended-recipe-card-img"
+                    src={
+                      images?.find((image) => image.recipe_id === recipe?.id)
+                        .image_url
+                    }
+                    alt="recipe card"
+                  ></img>
+                  <div className="recommended-recipe-card-title">
+                    {recipe?.title}
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
       <Footer />
